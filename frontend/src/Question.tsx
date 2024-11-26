6e49a8a431e1bde69359f6c66aea88038788d589
elchukc <clareelchuk@gmail.com>
2024-11-26 14:00:34 -0500

    consistent indentation across files

--- frontend/src/Question.tsx
+++ frontend/src/Question.tsx
@@ -2,32 +2,32 @@ import { FC } from "react"
 
 export type Question = { question: string; answers: string[] }
 type Props = {
-    questions: Question[];
-    onAnswer: (question: string, answer: string) => void;
+  questions: Question[];
+  onAnswer: (question: string, answer: string) => void;
 }
 
 export const QuestionCard: FC<Props> = (props) => {
 
-    return (
-        <>
-            {props.questions.map((e, i) => {
+  return (
+    <>
+      {props.questions.map((e, i) => {
+        return (
+          <div className="card" key={`${e.question}-${i}`}>
+            <h2>{e.question}</h2>
+            <ul>
+              {e.answers.map((a, j) => {
                 return (
-                    <div className="card" key={`${e.question}-${i}`}>
-                        <h2>{e.question}</h2>
-                        <ul>
-                            {e.answers.map((a, j) => {
-                                return (
-                                    <li key={`${e.question}-choice-${j}`}>
-                                        <button onClick={() => props.onAnswer(e.question, a)}>
-                                            {a}
-                                        </button>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </div>
+                  <li key={`${e.question}-choice-${j}`}>
+                    <button onClick={() => props.onAnswer(e.question, a)}>
+                      {a}
+                    </button>
+                  </li>
                 )
-            })}
-        </>
-    )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </>
+  )
 }
--- scraper/db.py
+++ scraper/db.py
@@ -3,31 +3,31 @@ from pymongo.server_api import ServerApi
 from flask import current_app, g
 
 def connect_db():
-    try:
-        # Create a new client and connect to the server
-        if 'db' not in g:
-            client = MongoClient(current_app.config['CONNECTION_STRING'], server_api=ServerApi('1'))
-            result = client.admin.command('ping')
+  try:
+    # Create a new client and connect to the server
+    if 'db' not in g:
+      client = MongoClient(current_app.config['CONNECTION_STRING'], server_api=ServerApi('1'))
+      result = client.admin.command('ping')
 
-            if int(result.get('ok')) == 1:
-                print("Connected to db.")
-            else:
-                raise Exception("Cluster ping returned OK != 1")
+      if int(result.get('ok')) == 1:
+        print("Connected to db.")
+      else:
+        raise Exception("Cluster ping returned OK != 1")
 
-            g.db = client
-        return g.db
+      g.db = client
+    return g.db
 
-    except Exception as e:
-        print(e)
+  except Exception as e:
+    print(e)
 
 
 def close_db(e = None):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
+  db = g.pop('db', None)
+  if db is not None:
+    db.close()
 
 
 def init_app(app):
-    app.teardown_appcontext(close_db)
+  app.teardown_appcontext(close_db)
 
 
